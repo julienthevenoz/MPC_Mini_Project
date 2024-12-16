a7a954ae9c4b17f22b90a30874c7e4bdf8a0a449
JulienThevenoz <julien@thevenoz.ch>
2024-12-16 15:21:47 +0100

    manquait la déf de mpc_lon et mpc_lat

--- testdelahess/car_project/testlongitude.m
+++ testdelahess/car_project/testlongitude.m
@@ -5,6 +5,9 @@ sys = car.linearize(xs, us);
 [sys_lon, sys_lat] = car.decompose(sys);
 % Design MPC controller
 H_lon = 10; % Horizon length in seconds
+mpc_lon = MpcControl_lon(sys_lon, Ts, H_lon);
+mpc_lat = MpcControl_lat(sys_lat, Ts, H_lon);
+
 % Get control input for longitudinal subsystem
 
 %u_lon = mpc_lon.get_u(x_lon, ref_lon);%DEBUG : see what happens if we give state 80km/h and ref 120km/h
