d1a0650c5fa1dcad14ba14aad0a932744e9ed18b
EstB2003 <estelle.baumann@epfl.ch>
2024-12-27 14:52:52 +0100

    çamarche?

---
+++ car_project/nmpc_test.m
@@ -0,0 +1,16 @@
+H = 15;
+Ts = 1/10; % Sample time
+car = Car(Ts);
+mpc = NmpcControl(car, H);
+x0 = [0 0 0 80/3.6]';
+ref = [3 100/3.6]';
+
+params = {};
+params.Tf = 15;
+params.myCar.model = car;
+params.myCar.x0 = [0 0 0 80/3.6]'; params.myCar.u = @mpc.get_u;
+ref1 = [0 80/3.6]';
+ref2 = [3 100/3.6]';
+params.myCar.ref = car.ref_step(ref1, ref2, 2);
+result = simulate(params);
+visualization(car, result);
