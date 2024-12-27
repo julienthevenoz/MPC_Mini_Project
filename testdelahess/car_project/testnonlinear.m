ea1ab73c84009a597d020da3e3c5279e2a4958d9
Johanne <johanne.pinel@epfl.ch>
2024-12-27 11:09:25 +0100

    debut part 6

---
+++ testdelahess/car_project/testnonlinear.m
@@ -0,0 +1,26 @@
+close all;
+clear all;
+
+Ts = 1/10; % Sample time
+car = Car(Ts);
+[xs, us] = car.steady_state(120 / 3.6); 
+sys = car.linearize(xs, us);
+[sys_lon, sys_lat] = car.decompose(sys);
+% Design MPC controller
+H_lon = 15; % Horizon length in seconds
+% Get control input for longitudinal subsystem
+
+mpc_lon = MpcControl_lon(sys_lon, Ts, H_lon);
+mpc_lat = MpcControl_lat(sys_lat, Ts, H_lon);
+mpc = car.merge_lin_controllers(mpc_lon, mpc_lat);
+
+params = {};
+params.Tf = 15;
+params.myCar.model = car;
+params.myCar.x0 = [0 0 0 80/3.6]';
+params.myCar.u = @mpc.get_u;
+ref1 = [0 80/3.6]';
+ref2 = [3 100/3.6]';
+params.myCar.ref = car.ref_step(ref1, ref2, 2);
+result = simulate(params);
+visualization(car, result);
