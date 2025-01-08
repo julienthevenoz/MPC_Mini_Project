a8a5998ad5c5b2762a10dfbc34e67e8186902922
JulienThevenoz <julien@thevenoz.ch>
2025-01-08 14:32:11 +0100

    jsp

--- car_project/templates/NmpcControl.m
+++ car_project/templates/NmpcControl.m
@@ -23,7 +23,7 @@ classdef NmpcControl < handle
         % to debug via
         %   nmpc.sol.value(nmpc.X)
         % 
-        X, U
+        X, U, pL, p
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
     end
@@ -48,43 +48,72 @@ classdef NmpcControl < handle
             obj.x0other = opti.parameter(nx, 1);  % initial state of other car
 
             % SET THIS VALUE TO BE YOUR CONTROL INPUT
-            % obj.u0 = opti.variable(nu, 1);
+            obj.u0 = opti.variable(nu, 1);
+         
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
-
-            h = car.Ts;   %h is the sample period of RK4. What should it be ?
-            %define discrete time dynamics function (integration : x+ = f(x,u))
-            f_discrete  = @(x,u) RK4(x,u,h,@car.f);  %we give it the car.f continuous time dynamics to RK4 to be integrated
+            h = car.Ts;
+            f_discrete  = @(x,u) RK4(x,u,h,@car.f);
 
             % Define your problem using the opti object created above
-            %define decision variables
+
+             %define decision variables
             obj.X = opti.variable(nx,N+1); % state trajectory variables
             obj.U = opti.variable(nu,N);   % control trajectory (throttle, brake)
-            obj.u0 = obj.U(:,1);
-
-
-    
-            %define the cost to minimize
-            opti.minimize(...
-              1000*(obj.X(4,:) - obj.ref(2))*(obj.X(4,:) - obj.ref(2))'  + ... % minimize difference between V and V_ref
-              1000*(obj.X(2,:) - obj.ref(1))*(obj.X(2,:) - obj.ref(1))' + ... %minimize difference between y and y_ref
-              1*obj.U(1,:)*obj.U(1,:)' + ... % Minimize steering
-              1*obj.U(2,:)*obj.U(2,:)' );% Minimize throttle
-             
-            % initial constraints
-            opti.subject_to(obj.X(:,1) == obj.x0);
-
-            %time dynamics equality constraints
-            for i=1:N
-                opti.subject_to(obj.X(:,i+1) == f_discrete(obj.X(:,i), obj.U(:,i)));
-            end
+            obj.p = opti.variable(2, N+1);  %define p, the [x and y] of blue car (yes it's superfluous I know)
+            %obj.u0 = obj.U(:,1);
+            obj.pL = opti.variable(2,N+1); %pL is [x,y] of red car
+            pL0 = [obj.x0other(1) ; obj.x0other(2)]; %initial state of red car
+            V_red = obj.x0other(4); %speed of the red car (it is constant)
             
-            %inequality constraints
-            opti.subject_to(-1 <= obj.U(2,:) <= 1);  %limit throttle
-            opti.subject_to(-0.5236 <= obj.U(1,:) <= 0.5236); %limit steering to 30 degrees 
-            opti.subject_to(-0.5 <= obj.X(2,:) <= 3.5 ); % car mustn't go out of the road
-            opti.subject_to(-0.0873 <= obj.X(3,:) <= 0.0873);  %car angle theta must be smaller than 5 degrees
 
+             %define the cost to minimize
+            cost = ...
+             100*(obj.X(4,:) - obj.ref(2))*(obj.X(4,:) - obj.ref(2))'  + ... % minimize difference between V and V_ref
+             100*(obj.X(2,:) - obj.ref(1))*(obj.X(2,:) - obj.ref(1))' + ... %minimize difference between y and y_ref
+             0.01*obj.U(1,:)*obj.U(1,:)' + ... % Minimize steering
+             obj.U(2,:)*obj.U(2,:)' ;% Minimize throttle;
+
+            % change this line accordingly
+            %opti.subject_to( obj.u0 == 0 );
+
+            opti.subject_to(obj.U(:,1) == obj.u0);
+            opti.subject_to(obj.X(:,1) == obj.x0);
+            opti.subject_to(obj.pL(1,1) == pL0(1));  %initial x of red car is given 
+            opti.subject_to(obj.pL(2,:) == pL0(2));  %initial and all future y of red car are given (since red car doesn't change lane)
+            opti.subject_to(obj.p(1,:) == obj.X(1,:));  %define that p is simply x and y of blue car
+            opti.subject_to(obj.p(2,:) == obj.X(2,:));
+           
+
+            for k=1:N % loop over control intervals
+  
+                 opti.subject_to(obj.X(:,k+1) == f_discrete(obj.X(:,k), obj.U(:,k)));
+                 opti.subject_to(obj.pL(1,k+1) == obj.pL(1,k) + V_red * h);  %predict the position x of red car by integration  %NB the y position of red car never changes
+                 
+                % p = [obj.X(1,k) ; obj.X(2,k)];
+                % obj.pL = obj.pL + [obj.x0other(1) + obj.x0other(4)*h ; obj.x0other(2)];
+
+            end
+
+
+            H = [1/100 , 0 ;
+                0, 1/9 ];
+            
+            opti.subject_to(-1 <= obj.U(2,:));  %limit throttle
+            opti.subject_to(obj.U(2,:)  <= 1);
+            opti.subject_to(-0.5236 <= obj.U(1,:)); %limit steering to 30 degrees 
+            opti.subject_to(obj.U(1,:) <= 0.5236);
+            opti.subject_to(-0.5 <= obj.X(2,:)); % car mustn't go out of the road
+            opti.subject_to(obj.X(2,:)  <= 3.5 );
+            opti.subject_to(-0.0873 <= obj.X(3,:));  %car angle theta must be smaller than 5 degrees
+            opti.subject_to(obj.X(3,:) <= 0.0873);
+            
+            for k = 1:N+1
+                opti.subject_to((obj.p(:,k) - obj.pL(:,k))' * H * (obj.p(:,k) - obj.pL(:,k)) >= 1);  %apparently this needs to be in a for loop `?
+            end
+
+          
+             opti.minimize(cost);
 
             % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -119,7 +148,14 @@ classdef NmpcControl < handle
             obj.opti.set_value(obj.ref, ref);
             obj.opti.set_value(obj.x0other, x0other);
 
-            obj.sol = obj.opti.solve();   % actual solve
+            try
+                obj.sol = obj.opti.solve();   % Attempt to solve
+            catch err
+                disp('Solver failed!');
+                disp(err.message);
+                return;
+            end
+            disp("solve success in nmpcControl.solve")
             
             % Set warm start for next solve
             obj.opti.set_initial(obj.sol.value_variables());
--- car_project/test_NMPC.m
+++ car_project/test_NMPC.m
@@ -1,6 +1,6 @@
 clear all;
 close all;
-
+clc;
 
 
 Ts = 1/10; % Sample time
@@ -12,55 +12,56 @@ mpc = NmpcControl(car, H);
 % u = mpc.get_u(x0d ref); % check if the open−loop prediction is reasonable
 
 x0 = [0 0 0 80/3.6]'; % (x, y, theta, V) 
-ref1 = [0 80/3.6]'; % (y ref, V ref) 
-ref2 = [3 120/3.6]'; % (y ref, V ref)
 
-u0 = mpc.get_u(x0, ref2);
-u = mpc.sol.value(mpc.U);
-x = mpc.sol.value(mpc.X);
-
-t = tiledlayout(3,2);
-nexttile
-plot(x(2,:));
-ylabel('lane y position')
-xlabel('time [seconds/10]')
-title('Initial open-loop MPC computation : State x(2) [y position]')
-nexttile
-
-plot(x(3,:));
-ylabel('theta')
-xlabel('time [seconds/10]')
-title('Initial open-loop MPC computation : State x(3) [theta]')
-nexttile
-
-plot(x(4,:));
-ylabel("Speed V")
-xlabel('time [seconds/10]')
-title('Initial open-loop MPC computation state x(4) [Speed]')
-nexttile
-
-plot(u(1,:));
-ylabel('steering')
-xlabel('time [seconds/10]')
-title('Initial open-loop MPC computation Input u(1) over time')
-nexttile
-
-plot(u(2,:));
-ylabel('throttle')
-xlabel('time [seconds/10]')
-title('Initial open-loop MPC computation Input u(2) over time')
-
-%%
+% u0 = mpc.get_u(x0, ref2);
 
 
 
+
+
+
+
+
+
+
+
+x0_ego = [0 0 0 80/3.6]';
+x0_other = [20 0 0 80/3.6]';
+ref1 = [0 80/3.6]';
+ref2 = [0 100/3.6]';
 params = {};
 params.Tf = 15;
 params.myCar.model = car;
-params.myCar.x0 = [0 0 0 80/3.6]';
+params.myCar.x0 = x0_ego;
 params.myCar.u = @mpc.get_u;
-ref1 = [0 80/3.6]';
-ref2 = [3 100/3.6]';
-params.myCar.ref = car.ref_step(ref1, ref2, 2);
+params.myCar.ref = car.ref_step(ref1, ref2, 1);
+params.otherCar.model = car;
+params.otherCar.x0 = x0_other;
+params.otherCar.u = car.u_const(80/3.6);
+
+
+%%plots
+% u = mpc.sol.value(mpc.U);
+% x = mpc.sol.value(mpc.X);
+pL = mpc.sol.value(mpc.pL);
+
+t = tiledlayout(3,2);
+nexttile
+plot(pL(1,:));
+title("Red car x position")
+% 
+% nexttile
+% plot(result.myCar.X(1,:));
+% title("Blue car x position")
+
+nexttile
+plot(pL(2,:));
+title("Red car y position")
+% 
+% nexttile
+% plot(result.myCar.X(2,:));
+% title("Blue car y position");
+
+%%
 result = simulate(params);
 visualization(car, result);
